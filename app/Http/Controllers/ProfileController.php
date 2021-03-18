bacebafbb4ea0a41ea2f3e620b1198136ca865f9
Kent <Kent.hsieh@104.com.tw>
2021-03-18 17:10:55 +0800

    update ProcfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,34 +5,69 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ProfileController extends Controller
 {
-    /**
-     * @param Request $request
-     * @return Factory|View
-     */
+    private $courseNameDict = [
+        'PHPbasic' => "A basic php course, including variable, class, function etc.",
+        'Laravel' => "Teaching about the best modern PHP frame ",
+        'CleanCode' => "How to code a everybody ok code",
+        'DesignPattern' => "How to solve the problem in various ways",
+        'Git' => "Version control software",
+        'Docker' => "A container-virtual-machine, lightweight, free, and with a cute whale when turning on",
+        'CICD' => "Introduction of Continuous integrate/Continuous deploy",
+        'SQL' => "Recognize database, teach the instruction like SELECT, WHERE, FROM etc.",
+        'AWS' => "a cloud technology",
+        'SearchEngine' => "Honest, I have no idea what it is",
+        'WebApplicationSecurity' => "How to prevent sql injection or something like that",
+        'Cryptography' => "basic encryption and decryption"
+    ]; //
     public function index(Request $request)
     {
+        # code...
+        if (request()->name == null) {
+            return view(
+                'profile',
+                [
+                    'name' => 'CourseProfile',
+                    'courseContent' => 'Wanna know what I am learning? Check the link !',
+                    'courseNameDict' => $this->courseNameDict,
+                ],
+            );
+        } elseif (!isset($this->courseNameDict[request()->name])) {
+            return Response::HTTP_NOT_FOUND . " NOT FOUND THIS COURSE";
+        }
         return view(
             'profile',
             [
-                'name' => 'Controller Name',
-                'id' => 1245,
-                'records' => [
-                    [
-                        'id' => 1,
-                        'text' => '1234',
-                    ],
-                    [
-                        'id' => 2,
-                        'text' => '1234',
-                    ],
-                ],
-            ]
+                'name' => request()->name,
+                'courseContent' => $this->courseNameDict[request()->name],
+                'courseNameDict' => $this->courseNameDict,
+            ],
         );
     }
 
+    public function apiIndex(Request $request)
+    {
+        if(request()->name == null || !isset($this->courseNameDict[request()->name])) {
+            return response()->json(
+                [
+                    'title' => "No Found at this path",
+                    'code' => 500, 
+                ],
+            );
+        } else {
+            return response()->json(
+                [
+                    'title' => request()->name,
+                    'courseContent' => $this->courseNameDict[request()->name],
+                ]
+            );
+        } 
+        
+    }
     /**
      * @param Request $request
      */
