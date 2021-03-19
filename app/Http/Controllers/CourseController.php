942e482a8102440bf6377dbe942936ece4bb8d3b
dnib <qazs0205@gmail.com>
2021-03-19 22:46:17 +0800

    Edit by teacher

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -7,4 +7,14 @@ use Illuminate\Http\Request;
 class CourseController extends Controller
 {
     //
+    public function apiAction()
+    {
+        /*
+        header();
+        echo json_encode(['name'=>'test']);
+        exit;*/
+        return response()->json([
+            'name' => 'php課程'
+        ]);
+    }
 }
